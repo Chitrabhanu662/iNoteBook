7e2c3fc2b94924be3a910842396cc23f9873ad28
Chitrabhanu <chitabhanuy@gmail.com>
2022-07-31 20:05:08 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,10 @@ app.use('/api/notes', require('./routes/notes'))
 const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static("/build"));
+    app.use(express.static("frontend/build"));
     const path = require("path");
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'build','index.html'));
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
     }
     )
 }
